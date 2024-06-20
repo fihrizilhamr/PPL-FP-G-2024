3eeed66df3fd93a6264c6a16ec83fa942946523b
Abhinaya Radiansyah Listiyanto <abhinaya8022@gmail.com>
2024-06-20 10:11:41 +0700

    update payment

--- PaymentService/create_payment.php
+++ PaymentService/create_payment.php
@@ -5,7 +5,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $pm_datetime = $_POST['pm_datetime'];
     $pm_totalprice = $_POST['pm_totlaprice'];
 
-    $sql = "INSERT INTO payment (pm_datetime, pm_totlaprice) VALUES ('$m_datetime', '$pm_totalprice')";
+    $sql = "INSERT INTO payment (pm_datetime, pm_totlaprice) VALUES ('$pm_datetime', '$pm_totalprice')";
 
     if ($conn->query($sql) === TRUE) {
         echo "New record created successfully";
